fa1c0552fc40a741569ca2fb1b6b70fd9fad2260
David Gipson <david.gipson@utexas.edu>
2022-07-15 14:33:47 -0500

    Add Player scene with movement, audio test scene

---
+++ Scripts/Player.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+	[Export]
+	private int _speed = 100;
+
+	private Vector2 _velocity = new Vector2();
+
+	public override void _Ready()
+	{
+		
+	}
+
+	public Vector2 GetInputVelocity()
+	{
+		var velocity = new Vector2();
+		if (Input.IsActionPressed("right"))
+		{
+			velocity.x += 1;
+		}
+		if (Input.IsActionPressed("left"))
+		{
+			velocity.x -= 1;
+		}
+		if (Input.IsActionPressed("down"))
+		{
+			velocity.y += 1;
+		}
+		if (Input.IsActionPressed("up"))
+		{
+			velocity.y -= 1;
+		}
+		return velocity.Normalized() * _speed;
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		_velocity = GetInputVelocity();
+		_velocity = MoveAndSlide(_velocity);
+	}
+
+}
