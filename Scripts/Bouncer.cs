ca2a8acbbc604ac59877f1c9989c4bb76b7bcf0e
Patrick C <patrickcaldwell1998@gmail.com>
2022-07-16 14:22:22 -0500

    Added poker table scene & updated scene file names
    Updated the naming scheme of the character scene files to match existing scenes

--- Scripts/Bouncer.cs
+++ Scripts/Bouncer.cs
@@ -3,36 +3,36 @@ using System;
 
 public class Bouncer : KinematicBody2D
 {
-    private Timer _timer;
+	private Timer _timer;
 
-    [Export] private float _rotateInterval = 2f;
+	[Export] private float _rotateInterval = 2f;
 
-    [Export] private float _speed = 80f;
+	[Export] private float _speed = 80f;
 
-    private readonly Vector2[] _dirs = { Vector2.Left, Vector2.Down, Vector2.Right, Vector2.Up };
-    
-    private int _dirIndex;
+	private readonly Vector2[] _dirs = { Vector2.Left, Vector2.Down, Vector2.Right, Vector2.Up };
+	
+	private int _dirIndex;
 
-    private Vector2 _curDir;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        _timer = GetNode<Timer>("Timer");
-        _timer.WaitTime = _rotateInterval;
-        _timer.Start();
-        _timer.Connect("timeout", this, nameof(OnTimerTick));
-        _curDir = _dirs[_dirIndex];
-    }
+	private Vector2 _curDir;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_timer = GetNode<Timer>("Timer");
+		_timer.WaitTime = _rotateInterval;
+		_timer.Start();
+		_timer.Connect("timeout", this, nameof(OnTimerTick));
+		_curDir = _dirs[_dirIndex];
+	}
 
-    public void OnTimerTick()
-    {
-        _dirIndex = (_dirIndex + 1) % 4;
-        _curDir = _dirs[_dirIndex];
-    }
+	public void OnTimerTick()
+	{
+		_dirIndex = (_dirIndex + 1) % 4;
+		_curDir = _dirs[_dirIndex];
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        MoveAndSlide(_curDir * _speed);
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		MoveAndSlide(_curDir * _speed);
+	}
 }
