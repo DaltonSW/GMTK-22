5e914d3eead99262a0f2446d7cab298fb44b45af
Patrick C <patrickcaldwell1998@gmail.com>
2022-07-16 17:52:25 -0500

    Title Screen

---
+++ Scenes/MainMenu.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class MainMenu : Control
+{
+	private AudioStreamPlayer audioPlayer;
+	private AudioStreamSample menuSong;
+
+	public override void _Ready()
+	{
+		menuSong = GD.Load<AudioStreamSample>("res://Assets/Sounds/MainMenu.wav");
+		audioPlayer = GetNode<AudioStreamPlayer>("AudioPlayer");
+		audioPlayer.Autoplay = true;
+		audioPlayer.Stream = menuSong;
+		audioPlayer.Play();
+	}
+
+	public void _on_StartButton_pressed()
+	{
+		GetTree().ChangeScene("res://Scenes/CardTable.tscn");
+	}
+
+	public void _on_QuitButton_pressed()
+	{
+		GetTree().Quit();
+	}
+
+	public void _on_CreditsButton_pressed()
+	{
+		GetTree().ChangeScene("res://Scenes/Credits.tscn");
+	}
+}
