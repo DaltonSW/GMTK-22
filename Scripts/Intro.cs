1b08776a995869d7563a3265fb6443ed1331d03e
Dalton <daltonstwilliams@gmail.com>
2022-07-17 12:21:34 -0500

    Intro script created

---
+++ Scripts/Intro.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class Intro : Node2D
+{
+    private Player _player;
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _player = GetNode<Player>("Player");
+        _player.Visible = true;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if (!Input.IsActionJustPressed("ui_select")) return;
+        if (_player.AdjacentToObjective)
+        {
+            _player.Visible = false;
+            GetTree().ChangeScene("res://Scenes/Scene.tscn");
+        }   
+    }
+}
