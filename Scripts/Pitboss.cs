85bc3afd4710a50e7b71830cbcb6573468338f0c
David Gipson <david.gipson@utexas.edu>
2022-07-16 20:50:27 -0500

    Add pit boss AI

--- Scripts/Pitboss.cs
+++ Scripts/Pitboss.cs
@@ -3,19 +3,54 @@ using System;
 
 public class Pitboss : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	private Timer _phoneTimer;
+	private Timer _alertTimer;
 
-	// Called when the node enters the scene tree for the first time.
+	private Area2D _fieldOfView;
+
+	private AnimatedSprite _sprite;
+
+	private bool _alert;
+
+	[Signal]
+	public delegate void DetectedPlayer();
+
 	public override void _Ready()
 	{
-		
+		_sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+		_phoneTimer = GetNode<Timer>("PhoneTimer");
+		_alertTimer = GetNode<Timer>("AlertTimer");
+		_fieldOfView = GetNode<Area2D>("FieldOfView");
+
+		OnAlertTimerTimeout();
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public void OnPhoneTimerTimeout()
+	{
+		_alert = true;
+		_sprite.Play("Idle Down");
+		_alertTimer.Start();
+	}
+
+	public void OnAlertTimerTimeout()
+	{
+		_alert = false;
+		_sprite.Play("Check Phone");
+		_phoneTimer.Start();
+	}
+
+	public void OnFieldOfViewBodyEntered(Node body)
+	{
+		if (_alert && body.IsInGroup("player"))
+		{
+			EmitSignal(nameof(DetectedPlayer));
+		}
+	}
+
+	private static int positiveMod(int dividend, int mod) => ((dividend % mod) + mod) % mod;
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+	}
 }
