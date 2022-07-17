4a017de88b478a51fc86c8bea14144dbb87a7877
Dalton <daltonstwilliams@gmail.com>
2022-07-17 10:08:57 -0500

    Renamed some stuff

--- Scripts/CardTable.cs
+++ Scripts/CardTable.cs
@@ -4,18 +4,18 @@ using System;
 public class CardTable : Node2D
 {
 	private Node2D _dealer;
-	private Sprite _exclam;
+	private Sprite _exclamation;
 
 	public override void _Ready()
 	{
 		_dealer = GetNode<Node2D>("Dealer");
-		_exclam = GetNode<Sprite>("ExclamPoint");
+		_exclamation = GetNode<Sprite>("ExclamationPoint");
 	}
 
 	public void RemoveDealer()
 	{
 		_dealer.QueueFree();
 		_dealer = null;
-		_exclam.Visible = false;
+		_exclamation.QueueFree();
 	}
 }
